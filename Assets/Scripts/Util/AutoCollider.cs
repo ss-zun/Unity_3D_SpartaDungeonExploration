c9ba670abe078b39dd03766c684699de329f04da
ss-zun <zxq0158@gmail.com>
2024-05-28 22:32:21 +0900

    Init
    맵 프리팹 추가, 캐릭터 프리팹 추가
    여러 셋팅

---
+++ Assets/Scripts/Util/AutoCollider.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AutoCollider : MonoBehaviour
+{
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        Renderer mr = GetComponentInChildren<MeshRenderer>();
+        if (mr == null) mr = GetComponentInChildren<SkinnedMeshRenderer>();
+        var box = GetComponentInChildren<BoxCollider>();
+        box.center = mr.bounds.center - transform.position;
+        box.size = mr.bounds.size;
+        UnityEditor.EditorApplication.delayCall += () =>
+        {
+            DestroyImmediate(this);
+        };
+    }
+#endif
+}
