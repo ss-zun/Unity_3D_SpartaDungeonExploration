9ba9b642d41ec0b45a8f1c25847007bfaa00e44f
ss-zun <zxq0158@gmail.com>
2024-05-29 16:51:31 +0900

    Feature : 애니메이션 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     private Look look;
 
     private Rigidbody rb;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+
         movement = GetComponent<Movement>();
         jump = GetComponent<Jump>();
         look = GetComponent<Look>();
@@ -34,19 +37,22 @@ public class PlayerController : MonoBehaviour
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
-        {
+        {           
             movement.curMovementInput = context.ReadValue<Vector2>();
+            animator.SetBool("isWalk", true);
         }
         else if (context.phase == InputActionPhase.Canceled)
         {
             movement.curMovementInput = Vector2.zero;
+            animator.SetBool("isWalk", false);
         }
     }
 
     public void OnJumpInput(InputAction.CallbackContext context)
-    {
+    {     
         if (context.phase == InputActionPhase.Started && jump.IsGrounded())
         {
+            animator.SetTrigger("isJump");
             rb.AddForce(Vector2.up * jump.jumpForce, ForceMode.Impulse);
         }
     }
